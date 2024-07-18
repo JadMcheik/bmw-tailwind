9e2e1b80b1dfa0917d056fc001c87f6638755759
JadMcheik <jad.m.mcheik@gmail.com>
2024-07-18 13:39:09 +0300

    navbar tailwind done

---
+++ components/Banner.js
@@ -0,0 +1,27 @@
+
+
+
+const Banner = () => {
+    return ( 
+
+
+
+
+        <div className={styles.banner_car}>
+        <div className={styles.image_container_text}>
+
+          <div>
+            <h1>Welcome to our website</h1>
+          </div>
+  
+          <div >
+            <h3>Rely on us to choose for you the best car you can get!</h3>
+          </div>
+        </div>
+      </div>
+
+
+     );
+}
+ 
+export default Banner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
+
+
+
+
+
+
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +15,37 @@ module.exports = {
   ],
   theme: {
     extend: {
+
+
+      screens: {
+
+
+'min-1500':{'min' : '1500px'},
+
+'max-1300':{'max':'1300px'},
+'max-700':{'max':'700px'},
+'max-500':{'max':'500px'},
+'max-400':{'max':'400px'}
+      },
+
+
+
+
+      fontFamily: {
+        customFont: ['"Custom Font"', "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+      "Lucida Sans Unicode", 'Geneva', 'Verdana', 'sans-serif'],
+        // Add more custom font families as needed
+      },
+
+
+      colors: { 
+        'custom-dark-blue': '#272727', 
+        'navbar-bg-color':'#f0f2f5',
+        'link-color':'#606060',
+        'menu-link-hover':'#12436b',
+    }, 
+
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
